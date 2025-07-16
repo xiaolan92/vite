b18e8fc41860a8bd111a6c8b769639db2f84d27a
xiaolan92 <13262631372@163.com>
2025-07-16 23:10:18 +0800

    feat: 添加组件定位插件

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useRoutes } from 'react-router-dom';
-import { App } from '@/App';
+import {useRoutes} from 'react-router-dom';
+import {App} from '@/App';
 
 const routeList = [
-    { path: '/', element: <App /> }
+    {path: '/', element: <App/>},
 ]
 
 export default function routes() {
-  return useRoutes(routeList);
+    return useRoutes(routeList);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 import {resolve} from "path";
 
 export default defineConfig({
-
+    base: './',
     resolve: {
         alias: {
             "@": resolve(__dirname, "./src")
@@ -12,7 +12,31 @@ export default defineConfig({
     },
 
     plugins: [
-        react(),
+        react({
+            babel: {
+                plugins: [
+                    // other Babel plugins
+                    [
+                        "@locator/babel-jsx/dist",
+                        {
+                            env: "development",
+                        },
+                    ],
+                ],
+            },
+        }),
         tailwindcss(),
     ],
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true
+    },
+    devServer: {
+        historyApiFallback: {
+            rewrites: [
+                {from: /\/vite-project/, to: '/vite-project/index.html'}
+            ]
+        }
+
+    }
 })
